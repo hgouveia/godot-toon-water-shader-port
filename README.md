996462efea1038adb2501793143148730ff3ffe5
Jose De Gouveia <dhgouveia@gmail.com>
2021-06-30 12:54:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Toon Water Shader
+
+Ported from original Unity [Roystan's Toon Water Shader](https://roystan.net/articles/toon-water.html) to Godot 3.X
+
+![](preview.png)
+
+## Resources:
+- Roystan's Article: https://roystan.net/articles/toon-water.html
+- Miziziziz Depth Buffer: https://youtu.be/PLCGL3RW548?t=1164
---
+++ Scripts/ObjectFloating.gd
@@ -0,0 +1,19 @@
+extends Spatial
+
+
+var initialPosition:Vector3;
+var offset:float;
+var height:float = 0.1;
+var period:float = 1.0;
+var time:float = 1.0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	initialPosition = get_translation()
+	offset = 1 - (randf() * 2)
+
+
+func _process(delta):
+	time += delta;
+	set_translation(initialPosition - Vector3.UP * sin((time + offset) * period) * height);
+	
